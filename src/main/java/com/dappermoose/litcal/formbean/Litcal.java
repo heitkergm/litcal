66bde56e80d7c7c74cc7669b96cfae6b2210f0bd
Matt Heitker <heitkergm@acm.org>
2016-10-02 23:04:34 -0400

    use lombok annotations everywhere

--- src/main/java/com/dappermoose/litcal/formbean/Litcal.java
+++ src/main/java/com/dappermoose/litcal/formbean/Litcal.java
@@ -3,32 +3,17 @@ package com.dappermoose.litcal.formbean;
 import javax.validation.constraints.Min;
 import javax.validation.constraints.NotNull;
 
+import lombok.Getter;
+import lombok.Setter;
+
 /**
  * The Class Litcal.
  */
+@Getter
+@Setter
 public class Litcal
 {
     @NotNull (message = "{litcal.year.required}")
     @Min (value = 2000, message = "{litcal.year.min}")
     private Integer year;
-
-    /**
-     * Gets the year.
-     *
-     * @return the year
-     */
-    public Integer getYear ()
-    {
-        return year;
-    }
-
-    /**
-     * Sets the year.
-     *
-     * @param yearNew the new year value
-     */
-    public void setYear (final Integer yearNew)
-    {
-        year = yearNew;
-    }
 }
