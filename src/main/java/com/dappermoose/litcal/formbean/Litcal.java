ddbb03d9c85b6d868456b15a54b4e44fba7517f3
Matt Heitker <heitkergm@acm.org>
2017-01-27 14:41:25 -0500

    fix lombok javadoc

--- src/main/java/com/dappermoose/litcal/formbean/Litcal.java
+++ src/main/java/com/dappermoose/litcal/formbean/Litcal.java
@@ -7,12 +7,18 @@ import lombok.Getter;
 import lombok.Setter;
 
 /**
- * The Class Litcal.
+ * The Litcal formbean.
  */
 @Getter
 @Setter
 public class Litcal
 {
+    /**
+     * The calendar year.
+     *
+     * @param year for the liturgical calendar
+     * @return the year
+     */
     @NotNull (message = "{litcal.year.required}")
     @Min (value = 2000, message = "{litcal.year.min}")
     private Integer year;
