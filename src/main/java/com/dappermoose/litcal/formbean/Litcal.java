765132d9b4dd0036177e8aab967fde5951173987
Matt Heitker <heitkergm@acm.org>
2018-09-30 15:48:09 -0400

    update versions;
    add lombok equals and hashcode to all beans

--- src/main/java/com/dappermoose/litcal/formbean/Litcal.java
+++ src/main/java/com/dappermoose/litcal/formbean/Litcal.java
@@ -3,6 +3,7 @@ package com.dappermoose.litcal.formbean;
 import javax.validation.constraints.Min;
 import javax.validation.constraints.NotNull;
 
+import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -11,6 +12,7 @@ import lombok.Setter;
  */
 @Getter
 @Setter
+@EqualsAndHashCode
 public class Litcal
 {
     /**
