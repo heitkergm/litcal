347fb6c3d3b65bc7a13d37ce776d0b582afa5f0b
Matt Heitker <heitkergm@acm.org>
2025-12-16 17:32:58 -0500

    Updates
    byte-buddy 1.18.5
    checkstyle 13.3.0
    classgraph 4.8.184
    classmate 1.7.3
    dependency-check-maven 12.2.0
    font-awesome 7.2.0
    groovy 5.0.3 *** downgraded to 5.0.3 due to bug. s/b fixed in 5.0.5 ***
    jboss-logging 3.6.2.Final
    jquery 4.0.0
    jquery-validation 1.22.0
    logback 1.5.29
    maven 3.9.12
    maven-compiler-plugin 3.15.0
    maven-dependency-plugin 3.10.0
    maven-surefire-plugin 3.5.5
    micrometer 1.16.2
    plexus-compiler-api 2.16.2
    plexus-compiler-javac 2.16.2
    pmd 7.22.0
    spring-boot 4.0.3
    tomcat 11.0.15
    versions-maven-plugin 2.21.0
    webjars-locator-lite 1.1.3
    
    declare lombok as an annotation processor
    
    invoke nu.validator via the nu:validator plugin, NOT directly

--- src/main/java/com/dappermoose/litcal/init/SpringWebConfig.java
+++ src/main/java/com/dappermoose/litcal/init/SpringWebConfig.java
@@ -5,12 +5,13 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.support.ReloadableResourceBundleMessageSource;
-import org.springframework.lang.NonNull;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.i18n.CookieLocaleResolver;
 import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
+
+import org.jspecify.annotations.NonNull;
 
 import nz.net.ultraq.thymeleaf.layoutdialect.LayoutDialect;
 
